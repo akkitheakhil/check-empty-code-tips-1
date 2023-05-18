7a53bb95e29f06e6b63605a09d9b6aa955170f1b
Akhil Padmanabhan <speaktoakhilp@gmail.com>
2023-05-18 15:02:39 +0200

    UPDATE: use generic for strict checking

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,59 @@
-import { isEmptyData } from './utils/is-empty';
+import { isDataNotEmpty } from "./utils/is-empty";
 const formBang: HTMLFormElement = document.querySelector("form#bang");
-const inputBang: HTMLInputElement = document.querySelector("input#level-input-bang");
+const inputBang: HTMLInputElement = document.querySelector(
+  "input#level-input-bang"
+);
 const errorBang: HTMLSpanElement = document.querySelector("span.error-bang");
-const successBang: HTMLSpanElement = document.querySelector("span.success-bang");
+const successBang: HTMLSpanElement =
+  document.querySelector("span.success-bang");
 
 const formUtil: HTMLFormElement = document.querySelector("form#util");
-const inputUtil: HTMLInputElement = document.querySelector("input#level-input-util");
+const inputUtil: HTMLInputElement = document.querySelector(
+  "input#level-input-util"
+);
 const errorUtil: HTMLSpanElement = document.querySelector("span.error-util");
-const successUtil: HTMLSpanElement = document.querySelector("span.success-util");
-
+const successUtil: HTMLSpanElement =
+  document.querySelector("span.success-util");
 
 const SOFTWARE_ENGINEER_LEVEL_MAP: Record<number, string> = {
-    0: "Junior Software Engineer",
-    1: "Software Engineer",
-    2: "Senior Software Engineer",
-    3: "Staff Engineer",
-    4: "Principal Engineer",
-    5: "Software architect"
-}
+  0: "Junior Software Engineer",
+  1: "Software Engineer",
+  2: "Senior Software Engineer",
+  3: "Staff Engineer",
+  4: "Principal Engineer",
+  5: "Software architect",
+};
 
 formBang.addEventListener("submit", (e: SubmitEvent) => {
-    e.preventDefault();
-    successBang.textContent = "";
-    errorBang.textContent = "";
-    const softwareEngineerLevel: number = inputBang.valueAsNumber;
+  e.preventDefault();
+  successBang.textContent = "";
+  errorBang.textContent = "";
+  const softwareEngineerLevel: number = inputBang.valueAsNumber;
 
-    if (!softwareEngineerLevel) {
-        errorBang.textContent = 'Please enter a value';
-        return;
-    }
+  if (!softwareEngineerLevel) {
+    errorBang.textContent = "Please enter a value";
+    return;
+  }
 
-    if (softwareEngineerLevel >= 0 && softwareEngineerLevel <= 5) {
-        const title = SOFTWARE_ENGINEER_LEVEL_MAP[softwareEngineerLevel];
-        successBang.textContent = `Your title is ${title}`
-    }
+  if (softwareEngineerLevel >= 0 && softwareEngineerLevel <= 5) {
+    const title = SOFTWARE_ENGINEER_LEVEL_MAP[softwareEngineerLevel];
+    successBang.textContent = `Your title is ${title}`;
+  }
 });
-
-
 
 formUtil.addEventListener("submit", (e: SubmitEvent) => {
-    e.preventDefault();
-    errorUtil.textContent = "";
-    successUtil.textContent = "";
-    const softwareEngineerLevel: number = inputUtil.valueAsNumber;
+  e.preventDefault();
+  errorUtil.textContent = "";
+  successUtil.textContent = "";
+  const softwareEngineerLevel: number = inputUtil.valueAsNumber;
 
-    if (isEmptyData(softwareEngineerLevel)) {
-        errorUtil.textContent = 'Please enter a value from 0 - 5';
-        return;
-    }
+  if (!isDataNotEmpty(softwareEngineerLevel)) {
+    errorUtil.textContent = "Please enter a value from 0 - 5";
+    return;
+  }
 
-    if (softwareEngineerLevel >= 0 && softwareEngineerLevel <= 5) {
-        const title = SOFTWARE_ENGINEER_LEVEL_MAP[softwareEngineerLevel];
-        successUtil.textContent = `Your title is ${title}`
-    }
+  if (softwareEngineerLevel >= 0 && softwareEngineerLevel <= 5) {
+    const title = SOFTWARE_ENGINEER_LEVEL_MAP[softwareEngineerLevel];
+    successUtil.textContent = `Your title is ${title}`;
+  }
 });
-
-
-
--- src/utils/is-empty.ts
+++ src/utils/is-empty.ts
@@ -1,21 +1,29 @@
 /**
- * @description isEmpty checks if the given data is empty/null/undefined.
+ * @description isDataNotEmpty, is generic typescript function that checks if the given data is empty/null/undefined.
  * It will check for all the object types.
  * @param value can be of type any.
- * @returns boolean - True if Given Data is Empty.
+ * @returns boolean - True if Given Data is not empty.
  */
-export const isEmptyData = (value: any): boolean => {
-    if (value === null) {
-        return true;
-    } else if (typeof value !== 'number' && value === '') {
-        return true;
-    } else if (value === 'undefined' || value === undefined) {
-        return true;
-    } else if (value !== null && typeof value === 'object' && !Object.keys(value).length) {
-        return true;
-    } else if (value !== null && Array.isArray(value) && !value.length) {
-        return true;
-    } else {
-        return false;
-    }
-};
+
+export function isDataNotEmpty<T>(value: T): value is NonNullable<T> {
+  if (value == null) {
+    return false;
+  } else if (
+    typeof value !== "number" &&
+    typeof value === "string" &&
+    value?.trim() === ""
+  ) {
+    return false;
+  } else if (value === undefined) {
+    return false;
+  } else if (
+    value !== null &&
+    typeof value === "object" &&
+    !Object.keys(value).length
+  ) {
+    return false;
+  } else if (value !== null && Array.isArray(value) && !value.length) {
+    return false;
+  }
+  return true;
+}
